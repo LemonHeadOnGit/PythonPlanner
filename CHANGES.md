87065cac9161a74a23535774120f3315c959a12f
rootinstinct <instinctadc@gmail.com>
2021-02-10 10:05:53 +0200

    Version 0.1.2
    BIG update in preparation for 0.1.2. More views! Formatting changes! Progress!

--- CHANGES.md
+++ CHANGES.md
@@ -1,6 +1,8 @@
-#   Version 0.1.1 Patch notes.
+#   Version 0.1.2 Patch notes.
 
-#   Changes to formatting, "Friday" has been changed to "Day 0" for clarity sake.
-#   Removal of several comments that are unneeded.
-#   Changes to grammatical and spelling errors.
-#   It now actually says *Better Timetable* instead of Timetable Tracker.
+#   ALOT OF CHANGES!
+#   Alot of progress made with easier configuration that doesn't require digging into code.
+#   You can now view your day in two different ways!, Normal view and Clear view, Clear view is still not polished. So expect more changes coming into either 0.1.3 or 0.2.
+#   New file, config.txt, basically the ground work for better user experience.
+#   It also now shows you if BetterTimetable is not configured and bugs you to change the code yourself.
+#   General formatting changes.
--- bettertimetable.py
+++ bettertimetable.py
@@ -1,17 +1,34 @@
-#   Version 0.1.1
-print("Better Timetable")
-print("What is the current day? Put 0 if it's a day outside of the 1-6 range.")  # Changing "Put 0 if it's Friday".
+#   BetterTimetable: Version 0.1.2
+#   Change for your personal timetable, remove any components if necessary but do not remove Day 1.
+#   Support for different configurations without modifying the code coming sometime later.
+print("BetterTimetable - Alpha 1")
+print()
+config = open("config.txt", "w")
+file = input("Would you like the Normal view (1) or [CONCEPT] Clear view. (0)\n")
+config.write(file)
+config.close()
+open1 = open("config.txt", "r")
+day1 = "[Day 1] (Unconfigured)"
+day2 = "[Day 2] (Unconfigured)"
+day3 = "[Day 3] (Unconfigured)"
+day4 = "[Day 4] (Unconfigured)"
+day5 = "[Day 5] (Unconfigured)"
+day6 = "[Day 6] (Unconfigured)"
+day0 = "[Day 0] (Unconfigured)"
+if day1 == "[Day 1] (Unconfigured)":
+    print("Status -- Unconfigured, please change code.")    #   If you're getting this error change Day 1 or configure your timetable.
+else: print("Select the day and we'll show you your schedule.")
 day = input()
 day = int(day)
-#   Change for your personal timetable, remove any components if necessary.
-#   Support for different configurations without modifying the code coming sometime later.
-day1 = "Day 1 (Unconfigured)"
-day2 = "Day 2 (Unconfigured)"
-day3 = "Day 3 (Unconfigured)"
-day4 = "Day 4 (Unconfigured)"
-day5 = "Day 5 (Unconfigured)"
-day6 = "Day 6 (Unconfigured)"
-day0 = "Day 0 (Unconfigured)"
+file = int(file)
+if file == 0: 
+    day1 = "[Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
+    day2 = "[Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
+    day3 = "[Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
+    day4 = "[Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
+    day5 = "[Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
+    day6 = "[Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
+    day0 = "[Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
 if day == 1:
     print(day1)
 if day == 2:
