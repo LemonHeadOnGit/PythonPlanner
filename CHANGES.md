e6a1817ddd182675b43fe51963f4e79731e33657
rootinstinct <instinctadc@gmail.com>
2021-02-11 08:01:11 +0200

    Bug Fix for 0.1.2
    Small bug fixes and updates to the CHANGES.md file, look at the that file for more information on BetterTimetable.

--- CHANGES.md
+++ CHANGES.md
@@ -1,8 +1,14 @@
 #   Version 0.1.2 Patch notes.
 
 #   ALOT OF CHANGES!
-#   Alot of progress made with easier configuration that doesn't require digging into code.
-#   You can now view your day in two different ways!, Normal view and Clear view, Clear view is still not polished. So expect more changes coming into either 0.1.3 or 0.2.
-#   New file, config.txt, basically the ground work for better user experience.
-#   It also now shows you if BetterTimetable is not configured and bugs you to change the code yourself.
-#   General formatting changes.
+   -- *Alot of progress made with easier configuration that doesn't require digging into code.*
+   -- *You can now view your day in two different ways!, Normal view and Clear view, Clear view is still not   polished. So expect more changes coming into either 0.1.3 or 0.2.*
+   -- *New file, config.txt, basically the ground work for better user experience.*
+   -- *It also now shows you if BetterTimetable is not configured and bugs you to change the code yourself.*
+   *General formatting changes.*
+
+#   What's next in 0.2
+
+   -- *A bit less work will be done as I research PyPi and learn how to implement GUI.. and just learn how to actually use python.*
+   -- *Which means being able to configure the timetable from the terminal and not the code may come as soon as next week considering we might be skipping 0.1.3 and just adding it into a cumalative 0.2.*
+   -- *And finally, it's going to take about 2 weeks at the maximum, but if it's released before that I wouldn't be surprised* 
--- bettertimetable.py
+++ bettertimetable.py
@@ -1,8 +1,6 @@
 #   BetterTimetable: Version 0.1.2
 #   Change for your personal timetable, remove any components if necessary but do not remove Day 1.
 #   Support for different configurations without modifying the code coming sometime later.
-print("BetterTimetable - Alpha 1")
-print()
 config = open("config.txt", "w")
 file = input("Would you like the Normal view (1) or [CONCEPT] Clear view. (0)\n")
 config.write(file)
@@ -15,6 +13,13 @@ day4 = "[Day 4] (Unconfigured)"
 day5 = "[Day 5] (Unconfigured)"
 day6 = "[Day 6] (Unconfigured)"
 day0 = "[Day 0] (Unconfigured)"
+print("BetterTimetable - Alpha 1")
+print()
+config = open("config.txt", "w")
+file = input("Would you like the Normal view (1) or [CONCEPT] Clear view. (0)\n")
+config.write(file)
+config.close()
+open1 = open("config.txt", "r")
 if day1 == "[Day 1] (Unconfigured)":
     print("Status -- Unconfigured, please change code.")    #   If you're getting this error change Day 1 or configure your timetable.
 else: print("Select the day and we'll show you your schedule.")
@@ -22,13 +27,13 @@ day = input()
 day = int(day)
 file = int(file)
 if file == 0: 
-    day1 = "[Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
-    day2 = "[Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
-    day3 = "[Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
-    day4 = "[Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
-    day5 = "[Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
-    day6 = "[Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
-    day0 = "[Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
+    day1 = "Day One: [Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
+    day2 = "Day Two: [Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
+    day3 = "Day Three: [Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
+    day4 = "Day Four: [Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
+    day5 = "Day Five: [Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
+    day6 = "Day Six: [Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
+    day0 = "Day Zero: [Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
 if day == 1:
     print(day1)
 if day == 2:
