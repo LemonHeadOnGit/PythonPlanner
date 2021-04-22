b177539b0b1c1bcbc29f97de79410a2298f7ac09
LemonHeadOnGit <41242131+LemonHeadOnGit@users.noreply.github.com>
2021-04-22 18:20:46 +0200

    Some code cleanup

--- menus.py
+++ menus.py
@@ -3,8 +3,16 @@
 
 #   ///DEFINE VARIOUS TYPES OF MENUS
 
-def mnPrompt(name,msg):
+def mnPrompt(name,msg): # This is a generic prompt menu.
     print("=====",name,"=====\n")
     print(msg+"\n")
     choice = input("CHOICE: ")
+    return choice
+
+def mnLaunch(TiPl): # This is the launch menu.
+    print("╔═══════════════╗\n║ PYTHONPLANNER ║\n╚═══════════════╝")
+    config = open("config.txt", "r")
+    print("===== MENU =====\n\n[1] CREATE NEW", TiPl, "\n[2] OPEN", TiPl, "\n[3] EDIT CONFIG FILE\n[4] EXIT")
+    config.close()
+    choice = input("\nCHOICE: ")
     return choice
--- pyplanner.py
+++ pyplanner.py
@@ -6,7 +6,7 @@
 
 import datetime
 import os
-import menus
+from menus import *
 
 #   ///CREATE VARIABLES
 
@@ -28,14 +28,14 @@ else:
 #   ///PROCEED WITH PLANNER SETUP
 
 if fExist == False:
-    cfgpar = input("Would you like the Normal [N] or (INDEV) Clear [C] view?\n=====\nCHOICE:")
+    cfgpar = input("Would you like the Normal [N] or (INDEV) Clear [C] view?\n=====\nCHOICE: ")
     print("=====\n")
     if cfgpar not in ["N","n","C","c"]:
         print("Invalid choice, defaulting to Normal.")
         cfgpar = "N"
     config.write(cfgpar)
     # -----
-    cfgpar = input("Would you like Timetable [T] or Planner [P] mode?\n=====\nCHOICE:")
+    cfgpar = input("Would you like Timetable [T] or Planner [P] mode?\n=====\nCHOICE: ")
     print("=====\n")
     if cfgpar not in ["T","t","P","p"]:
         print("Invalid choice, defaulting to Planner.")
@@ -50,11 +50,7 @@ if fExist == False:
 if TP in ["P","p"]:
     TiPl = "PLANNER"
 
-print("╔═══════════════╗\n║ PYTHONPLANNER ║\n╚═══════════════╝")
-config = open("config.txt", "r")
-print("===== MENU =====\n\n[1] CREATE NEW", TiPl, "\n[2] OPEN", TiPl, "\n[3] EDIT CONFIG FILE\n[4] EXIT")
-config.close()
-choice = input("\nCHOICE: ")
+choice = mnLaunch(TiPl)
 
 #   ///COMPUTE CHOICES
 
@@ -62,26 +58,18 @@ choice = int(choice)
 
 if choice == 1:
     print("-----UNDER CONSTRUCTION, EXITING-----")
+
 elif choice == 2:
     print("ENTERING OPEN MENU...")
+
 elif choice == 3:
     print("CLEARING CONFIG FILE...\n")
     rm("config.txt")
     print("DONE!\n")
     print("=== EXITING ===")
+
 elif choice == 4:
-    yn = menus.mnPrompt("WARNING","Are you sure you want to exit the script? [Y/N]")
+    yn = mnPrompt("WARNING","Are you sure you want to exit the script? [Y/N]")
     print("EXITING...")
+    
 else: print("Choice invalid, exiting...")
-
-#file = int(file)
-#if file == 0: 
-#    day1 = "Day One: [Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
-#    day2 = "Day Two: [Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
-#    day3 = "Day Three: [Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
-#    day4 = "Day Four: [Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
-#    day5 = "Day Five: [Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
-#    day6 = "Day Six: [Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
-#    day0 = "Day Zero: [Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
-
-#   End.
